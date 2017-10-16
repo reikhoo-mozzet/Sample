a965b767a53d206797a18321a3f200aca3b10a9e
Reikhoo <reikhoo.dev@gmail.com>
2017-10-16 17:41:54 +0900

    [Sample] BaseModel을 확장시킨 TypeAModel 구현.

---
+++ app/src/main/java/com/mozzet/sample/model/TypeAModel.java
@@ -0,0 +1,40 @@
+package com.mozzet.sample.model;
+
+import com.mozzet.sample.SampleAdapter;
+
+/**
+ * Created by reikhoo on 2017. 10. 16..
+ */
+
+public class TypeAModel extends BaseModel {
+
+    private String userName;
+    private String userImageUrl;
+
+    public TypeAModel(String userName, String userImageUrl) {
+        this.userName = userName;
+        this.userImageUrl = userImageUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserImageUrl() {
+        return userImageUrl;
+    }
+
+    public void setUserImageUrl(String userImageUrl) {
+        this.userImageUrl = userImageUrl;
+    }
+
+    @Override
+    public int getType() {
+        return SampleAdapter.TYPE_A;
+    }
+
+}
