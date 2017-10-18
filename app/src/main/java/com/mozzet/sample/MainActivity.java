aea8b8d7863368e598b2d145cc74355e0fb6abeb
Reikhoo <reikhoo.dev@gmail.com>
2017-10-18 17:36:37 +0900

    임시 커밋

--- app/src/main/java/com/mozzet/sample/MainActivity.java
+++ app/src/main/java/com/mozzet/sample/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends Activity {
         Log.e("main", "log");
         Toast.makeText(MainActivity.this,"wow",Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this,"poo",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"log2",Toast.LENGTH_LONG).show();
     }
 
     @Override
