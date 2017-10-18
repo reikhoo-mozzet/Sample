e8e0c5e2f37e87f0e1eb88a0eee19157ebee95a5
Reikhoo <reikhoo.dev@gmail.com>
2017-10-18 17:39:45 +0900

    [Sample] hotfix

--- app/src/main/java/com/mozzet/sample/MainActivity.java
+++ app/src/main/java/com/mozzet/sample/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends Activity {
         Toast.makeText(MainActivity.this,"wow",Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this,"poo",Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this,"log2",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"hotfix",Toast.LENGTH_LONG).show();
     }
 
     @Override
