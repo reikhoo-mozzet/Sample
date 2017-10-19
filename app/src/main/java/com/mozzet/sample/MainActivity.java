706ebefd0c4d28e39ff90885017962d3b92e4627
Reikhoo <reikhoo.dev@gmail.com>
2017-10-19 10:30:51 +0900

    test2-1

--- app/src/main/java/com/mozzet/sample/MainActivity.java
+++ app/src/main/java/com/mozzet/sample/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends Activity {
         Toast.makeText(MainActivity.this,"log2",Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this,"hotfix",Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this,"hotfix",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"test1",Toast.LENGTH_LONG).show();
     }
 
     @Override
